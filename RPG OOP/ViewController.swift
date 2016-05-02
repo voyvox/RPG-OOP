d4999a14a0e9e58b60a526c4b50c4cb4c7aca55b
Craig Newcomb <crgnewcomb@gmail.com>
2016-05-02 17:28:32 -0500

    + Created parent class Character, and sub classes Player and Enemy. Enemy has subclasses Kimara and Wizard.  + Added all Storyboard elements with auto layout.

--- RPG OOP/ViewController.swift
+++ RPG OOP/ViewController.swift
@@ -10,16 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var printLbl: UILabel!
+    
+    @IBOutlet weak var playerHpLbl: UILabel!
+    
+    @IBOutlet weak var enemyHpLbl: UILabel!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
+
 
